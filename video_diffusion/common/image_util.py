8c5945c4bd38456cd7a4204044a259d5d144641d
bryandlee <bryandlee@robotics.snu.ac.kr>
2023-01-10 11:10:01 +0000

    Feat: font download, save image gif

--- video_diffusion/common/image_util.py
+++ video_diffusion/common/image_util.py
@@ -1,3 +1,7 @@
+import os
+import requests
+from typing import Sequence
+
 import math
 import textwrap
 from PIL import Image, ImageDraw, ImageFont
@@ -5,16 +9,25 @@ from PIL import Image, ImageDraw, ImageFont
 
 IMAGE_EXTENSION = (".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm", ".tif", ".tiff", ".webp")
 
+FONT_URL = "https://raw.github.com/googlefonts/opensans/main/fonts/ttf/OpenSans-Regular.ttf"
+FONT_PATH = "/tmp/fonts/OpenSans-Regular.ttf"
 
-def pad(image: Image.Image, top=0, right=0, bottom=0, left=0, color=(255, 255, 255)):
+
+def pad(image: Image.Image, top=0, right=0, bottom=0, left=0, color=(255, 255, 255)) -> Image.Image:
     new_image = Image.new(image.mode, (image.width + right + left, image.height + top + bottom), color)
     new_image.paste(image, (left, top))
     return new_image
 
 
-def annotate_text(image: Image.Image, text: str, font_size=15):
-    font = ImageFont.truetype("/data/etc/OpenSans-VariableFont_wdth,wght.ttf", size=font_size)
+def download_font_opensans(path=FONT_PATH):
+    font_url = FONT_URL
+    response = requests.get(font_url)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "wb") as f:
+        f.write(response.content)
 
+
+def annotate_image_with_font(image: Image.Image, text: str, font: ImageFont.FreeTypeFont) -> Image.Image:
     image_w = image.width
     _, _, text_w, text_h = font.getbbox(text)
     line_size = math.floor(len(text) * image_w / text_w)
@@ -27,7 +40,14 @@ def annotate_text(image: Image.Image, text: str, font_size=15):
     return image
 
 
-def make_grid(images, rows=None, cols=None):
+def annotate_image(image: Image.Image, text: str, font_size: int = 15):
+    if not os.path.isfile(FONT_PATH):
+        download_font_opensans()
+    font = ImageFont.truetype(FONT_PATH, size=font_size)
+    return annotate_image_with_font(image=image, text=text, font=font)
+
+
+def make_grid(images: Sequence[Image.Image], rows=None, cols=None) -> Image.Image:
     if rows is None:
         assert cols is not None
         rows = math.ceil(len(images) / cols)
@@ -41,3 +61,20 @@ def make_grid(images, rows=None, cols=None):
             image = image.resize((w, h))
         grid.paste(image, box=(i % cols * w, i // cols * h))
     return grid
+
+
+def save_images_as_gif(
+    images: Sequence[Image.Image],
+    save_path: str,
+    loop=0,
+    duration=250,
+    optimize=False,
+) -> None:
+    images[0].save(
+        save_path,
+        save_all=True,
+        append_images=images[1:],
+        optimize=optimize,
+        loop=loop,
+        duration=duration,
+    )
